99721bd9cff8ee2cc6551f9025befafda5d657c7
Steve Lee <steve.lee@monotypeimaging.com>
2010-10-27 17:21:35 +0545

    Imports name changed to import

--- webfonts_admin.php
+++ webfonts_admin.php
@@ -94,7 +94,7 @@ foreach($projects_list as $project)
 <div id="poststuff">
 <div class="meta-box-sortables ui-sortable">
 <div class="postbox closed">
-	<h3 id="wfs_importProject"  style="cursor:pointer;"><span ><?php _e('Imports Projects'); ?></span><span style="display: block;" class=""></span></h3>
+	<h3 id="wfs_importProject"  style="cursor:pointer;"><span ><?php _e('Import Projects'); ?></span><span style="display: block;" class=""></span></h3>
     <div class="inside" >
     	<div id="imp_prj">
     <?php
